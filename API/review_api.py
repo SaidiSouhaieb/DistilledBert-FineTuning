8118564e57f787f18b185b2434630eaae5bc62e9
@Saidi-Souhaieb <Saidisouhaieb@takiacademyteam.com>
2024-02-01 09:21:36 +0100

    ds

--- API/review_api.py
+++ API/review_api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 # test
 
 # Souhaiebqsdsdssdssdsdsd
-# welyeysdsdsdsdssdsdsd
+# welyeysdsdsdsdssdsdsdsd
 
 from pydantic import BaseModel
 from transformers import DistilBertTokenizer
